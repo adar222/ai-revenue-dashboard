46e6d5ad5eb105c43db2c2bc1bb93408ebfae008
adar222 <adar222@gmail.com>
2025-06-21 20:21:15 +0300

    Create optimization.py

---
+++ optimization.py
@@ -0,0 +1,5 @@
+def show_optimization():
+    import streamlit as st
+    st.header("🚦 Product & Campaign Optimization")
+    st.info("This is the new optimization tab! Logic coming soon…")
+    # You’ll add your full logic here
