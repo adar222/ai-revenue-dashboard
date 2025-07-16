5aa20e55d1275fcf425b9dcd3cc163c9d96405c9
TheMioa <49129359+TheMioa@users.noreply.github.com>
2025-07-16 10:41:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from dashboard import show_dashboard
 from ivt_optimization import show_ivt_optimization
 from rpm_optimization import show_rpm_optimization
 from discrepancy_optimization import show_discrepancy_optimization
+from filter import show_filtering
+from pubimps import show_pubimps
+from IVT import show_IVT
 
 st.set_page_config(
     page_title="AI Revenue Dashboard",
@@ -21,6 +24,9 @@ with st.sidebar:
             "IVT Optimization",
             "RPM Optimization",
             "Discrepancy Optimization"
+            "Filtering low revenue and RPM",
+            "Pubimps/advimps discrepancy",
+            "IVT checker"
         ],
         index=0
     )
@@ -35,3 +41,9 @@ elif tab == "RPM Optimization":
     show_rpm_optimization()
 elif tab == "Discrepancy Optimization":
     show_discrepancy_optimization()
+elif tab == "Pubimps/advimps discrepancy":
+    show_pubimps()
+elif tab == "Filtering low revenue and RPM":
+    show_filtering()
+elif page == "IVT checker":
+    show_IVT()
