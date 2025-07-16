d9eaa880989bdf0d1320f096203b8d978940ad12
TheMioa <49129359+TheMioa@users.noreply.github.com>
2025-07-16 11:20:24 +0200

    Update pubimps.py

--- pubimps.py
+++ pubimps.py
@@ -9,7 +9,11 @@ def show_pubimps():
     uploaded_file = st.file_uploader("Upload a CSV or Excel file", type=["csv", "xlsx"])
 
     if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
+        if uploaded_file is not None:
+            if uploaded_file.name.endswith(".csv"):
+                df = pd.read_csv(uploaded_file)
+            else:
+                df = pd.read_excel(uploaded_file)
         st.success("File uploaded successfully.")
         
         # Ensure numeric types
